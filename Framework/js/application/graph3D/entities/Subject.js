c507926619fd598d1ac95bdd0b386c56ef4b925b
UsernameGospodiBlin <90669953+UsernameGospodiBlin@users.noreply.github.com>
2021-09-17 20:44:12 +0400

    Delete Framework directory

--- Framework/js/application/graph3D/entities/Subject.js
+++
@@ -1,7 +0,0 @@
-class Subject {
-    constructor(points = [], edges = [], polygones = []){
-        this.points = points;
-        this.edges = edges;
-        this.polygones = polygones;
-    }
-}
--- Framework/js/application/header/headerTemplate.js
+++
@@ -1,4 +0,0 @@
-Template.prototype.headerTemplate = () => `
-    <button class="headerBtn" id="showGraph2D">График 2D</button>
-    <button class="headerBtn" id="showGraph3D">Графики 3D</button>
-    <button class="headerBtn" id="showCalculator">Калькулятор</button>`;
